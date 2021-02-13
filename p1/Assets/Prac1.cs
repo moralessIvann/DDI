0f7fd67a92a86c679df8fb4d79ecd5fc35e9860a
Morales Ivan <imorales13@uabc.edu.mx>
2021-02-12 19:23:26 -0800

    updated files

---
+++ p1/Assets/Prac1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Prac1 : MonoBehaviour
+{
+    private void Start() {
+    
+    //int[] arreglo = {8,1,2,2,3}; //4,0,1,1,3
+    int[] arreglo = {4,0,5,2,9}; //2,0,3,1,4
+    FindNum(arreglo);
+
+    }
+
+    public void FindNum(int[] arreglo)
+    {
+        int cont = 0;
+
+        for(int i=0; i<5; i++){				
+            for(int j=0; j<5; j++){
+                if(arreglo[i] > arreglo[j]){
+                    cont++;
+                }
+            }
+            Debug.Log(cont);
+            cont = 0;
+        }
+    }
+
+    
+}
