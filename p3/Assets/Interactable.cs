dc6ab15958ad413309f3a84051071004c65bcf95
Morales Ivan <imorales13@uabc.edu.mx>
2021-02-28 18:42:51 -0800

    updated files

---
+++ p3/Assets/Interactable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject handPoint;
+    private GameObject pickedObject = null;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickedObject != null)
+        {
+            if(Input.GetKey("r")) //release
+            {
+                GetComponent<AudioSource>().Play();
+                pickedObject.GetComponent<Rigidbody>().useGravity = true;
+                pickedObject.GetComponent<Rigidbody>().isKinematic = false;
+                pickedObject.gameObject.transform.SetParent(null);
+                pickedObject = null;
+            }
+            
+        }
+    }
+
+    private void OnTriggerStay(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Objeto"))
+        {
+            if(Input.GetKey("g") && pickedObject == null) //grab
+            {
+                GetComponent<AudioSource>().Play();
+                other.GetComponent<Rigidbody>().useGravity = false;
+                other.GetComponent<Rigidbody>().isKinematic = true;
+                other.transform.position = handPoint.transform.position;
+                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
+                pickedObject = other.gameObject;
+            }
+        }
+
+        
+    }
+}
---
+++ p3/Assets/Prac3.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Prac3 : MonoBehaviour
+{
+    int[] nums = {6,7,9,5,2,3};
+    //int target = 7;
+    int target = 15;
+    
+    void Start()
+    {
+        string[] strArray = Array.ConvertAll((SumaDos(nums,target)), ele => ele.ToString());
+        Debug.Log(string.Join(",", strArray));
+    }
+
+    public int[] SumaDos(int[] nums, int target)
+    {   
+        int[] outArray = new int[2];
+
+        for(int i = 0; i<nums.Length; i++)
+        {
+            for(int j = 0; j<nums.Length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    outArray[0] = i;
+                    outArray[1] = j;
+                    break;
+                }
+            }
+        }
+        return outArray;
+    }
+}
